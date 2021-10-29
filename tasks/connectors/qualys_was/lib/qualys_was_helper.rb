45b88e49754a213f0f3f43bda1020ec4f86c752f
Chere Macbook <cheremac@Cheres-MacBook-Pro.local>
2021-10-29 16:45:27 +0530

    Changes as per task

--- tasks/connectors/qualys_was/lib/qualys_was_helper.rb
+++ tasks/connectors/qualys_was/lib/qualys_was_helper.rb
@@ -6,12 +6,12 @@ require "base64"
 module Kenna
   module Toolkit
     module QualysWasHelper
-      def qualys_was_get_token(username = 'username', password = 'Password')
+      def qualys_was_get_token(username, password)
         auth_details = "#{username}:#{password}"
         Base64::encode64(auth_details)
       end
 
-      def qualys_was_get_webapp(qualys_was_url = 'qualysapi.qg3.apps.qualys.com/qps/rest/3.0/')
+      def qualys_was_get_webapp(token, qualys_was_url = 'qualysapi.qg3.apps.qualys.com/qps/rest/3.0/')
         print "Getting Webapp \n"
         qualys_was_auth_api = "https://#{qualys_was_url}search/was/webapp"
         # auth_headers = { "content-type" => "application/json",
@@ -23,9 +23,16 @@ module Kenna
         @headers = {
           "Content-Type" => "application/json",
           "accept" => "application/json",
-          "Authorization" => "Basic #{qualys_was_get_token}"}
+          "Authorization" => "Basic #{token}"}
         payload = {
+           "ServiceRequest" => {
+              "preferences" => {
+                  "verbose" => "true",
+                  "limitResults": "100"
+              }
+           }
         }
+
 
         # print_debug "#{qualys_was_auth_api}"
         # print_debug "#{auth_headers}"
@@ -44,8 +51,8 @@ module Kenna
         response
       end
 
-      def qualys_was_get_webapp_findings(webapp_id = '55778935', qualys_was_url = 'qualysapi.qg3.apps.qualys.com/qps/rest/3.0/')
-        print "Getting Webapp Findings For #{webapp_id} \n"
+      def qualys_was_get_webapp_findings(webapp_id, token, qualys_was_url = 'qualysapi.qg3.apps.qualys.com/qps/rest/3.0/')
+        print_debug "Getting Webapp Findings For #{webapp_id} \n"
         qualys_was_auth_api = "https://#{qualys_was_url}search/was/finding"
         # auth_headers = { "content-type" => "application/json",
         #            "accept" => "application/json" }
@@ -56,7 +63,7 @@ module Kenna
         @headers = {
           "Content-Type" => "application/json",
           "accept" => "application/json",
-          "Authorization" => "Basic #{qualys_was_get_token}"}
+          "Authorization" => "Basic #{token}"}
 
         payload = {
           "ServiceRequest": {
@@ -91,7 +98,7 @@ module Kenna
         response
       end
 
-      def qualys_was_get_vuln(qids = ['150082'], qualys_was_url = 'qualysapi.qg3.apps.qualys.com/api/2.0/fo/')
+      def qualys_was_get_vuln(qids, token, qualys_was_url = 'qualysapi.qg3.apps.qualys.com/api/2.0/fo/')
         print "Getting Webapp Findings For #{qids} \n"
         qualys_was_auth_api = URI("https://#{qualys_was_url}knowledge_base/vuln/")
         # auth_headers = { "content-type" => "application/json",
@@ -103,7 +110,7 @@ module Kenna
         @headers = {
           "Content-Type" => "application/json",
           "accept" => "application/json",
-          "Authorization" => "Basic #{qualys_was_get_token}",
+          "Authorization" => "Basic #{token}",
           "X-Requested-With" => 'QualysPostman'
         }
 
@@ -129,129 +136,6 @@ module Kenna
         print auth_response.body
         print "\n\n \n\n"
         auth_response.body
-      end
-
-      def http_get(url, headers, max_retries = 5, verify_ssl = true)
-        RestClient::Request.execute(
-          method: :get,
-          url: url,
-          headers: headers,
-          verify_ssl: verify_ssl
-        )
-      rescue RestClient::TooManyRequests => e
-        puts "Exception! #{e}"
-        retries ||= 0
-        if retries < max_retries
-          retries += 1
-          sleep(15)
-          puts "Retrying!"
-          retry
-        end
-      rescue RestClient::UnprocessableEntity => e
-        puts "Exception! #{e}"
-      rescue RestClient::BadRequest => e
-        puts "Exception! #{e}"
-      rescue RestClient::InternalServerError => e
-        retries ||= 0
-        if retries < max_retries
-          retries += 1
-          sleep(15)
-          puts "Retrying!"
-          retry
-        end
-        puts "Exception! #{e}"
-      rescue RestClient::ServerBrokeConnection => e
-        puts "Exception! #{e}"
-      rescue RestClient::ExceptionWithResponse => e
-        puts "Exception! #{e}"
-        retries ||= 0
-        if retries < max_retries
-          retries += 1
-          puts "Retrying!"
-          sleep(15)
-          retry
-        end
-      rescue RestClient::NotFound => e
-        puts "Exception! #{e}"
-      rescue RestClient::Exception => e
-        puts "Exception! #{e}"
-        retries ||= 0
-        if retries < max_retries
-          retries += 1
-          sleep(15)
-          puts "Retrying!"
-          retry
-        end
-      rescue Errno::ECONNREFUSED => e
-        puts "Exception! #{e}"
-        retries ||= 0
-        if retries < max_retries
-          retries += 1
-          puts "Retrying!"
-          sleep(15)
-          retry
-        end
-      end
-
-      def http_post(url, headers, payload, max_retries = 5, verify_ssl = true)
-        RestClient::Request.execute(
-          method: :post,
-          url: url,
-          headers: headers
-        )
-      rescue RestClient::TooManyRequests => e
-        puts "Exception! #{e}"
-        retries ||= 0
-        if retries < max_retries
-          retries += 1
-          puts "Retrying!"
-          sleep(15)
-          retry
-        end
-      rescue RestClient::UnprocessableEntity => e
-        puts "Exception! #{e}"
-      rescue RestClient::BadRequest => e
-        puts "Exception! #{e}"
-      rescue RestClient::InternalServerError => e
-        puts "Exception! #{e}"
-        retries ||= 0
-        if retries < max_retries
-          retries += 1
-          puts "Retrying!"
-          sleep(15)
-          retry
-        end
-      rescue RestClient::ServerBrokeConnection => e
-        puts "Exception! #{e}"
-      rescue RestClient::ExceptionWithResponse => e
-        puts "Exception! #{e}"
-        retries ||= 0
-        if retries < max_retries
-          retries += 1
-          puts "Retrying!"
-          sleep(15)
-          retry
-        end
-      rescue RestClient::NotFound => e
-        puts "Exception! #{e}"
-      rescue RestClient::Exception => e
-        puts "Exception! #{e}"
-        retries ||= 0
-        if retries < max_retries
-          retries += 1
-          puts "Retrying!"
-          sleep(15)
-          retry
-        end
-      rescue Errno::ECONNREFUSED => e
-        puts "Exception! #{e}"
-        retries ||= 0
-        if retries < max_retries
-          retries += 1
-          puts "Retrying!"
-          sleep(15)
-          retry
-        end
       end
 
       def qualys_was_get_containers(qualys_was_url, token, pagesize, pagenum)
@@ -296,21 +180,3 @@ module Kenna
     end
   end
 end
-
-
-class A
-  extend Kenna::Toolkit::QualysWasHelper
-end
-
-web_apps = A.qualys_was_get_webapp
-web_apps_findings = {}
-web_apps['ServiceResponse']['data'].each do |web_app|
-  web_apps_findings[web_app['WebApp']['id']] = A.qualys_was_get_webapp_findings(web_app['WebApp']['id'])
-end
-web_apps_findings.each do |web_app, findings|
-  p "QID For #{web_app}"
-  qids = findings['ServiceResponse']['data'].map{|x| x['Finding']['qid']}
-
-  A.qualys_was_get_vuln(qids)
-end
-#A.qualys_was_get_vuln
--- tasks/connectors/qualys_was/qualys_was.rb
+++ tasks/connectors/qualys_was/qualys_was.rb
@@ -10,33 +10,33 @@ module Kenna
 
       def self.metadata
         {
-          id: "aqua",
-          name: "Aqua Vulnerabilities",
-          description: "Pulls assets and vulnerabilitiies from Aqua",
+          id: "qualys_was",
+          name: "qualys_was Vulnerabilities",
+          description: "Pulls assets and vulnerabilitiies from qualys_was",
           options: [
-            { name: "aqua_console",
+            { name: "qualys_was_console",
               type: "hostname",
               required: true,
               default: nil,
-              description: "Your Aqua Console hostname (without protocol and port), e.g. app.aquasecurity.com" },
-            { name: "aqua_console_port",
+              description: "Your qualys_was Console hostname (without protocol and port), e.g. app.qualys_wassecurity.com" },
+            { name: "qualys_was_console_port",
               type: "integer",
               required: false,
               default: nil,
-              description: "Your Aqua Console port, e.g. 8080" },
-            { name: "aqua_user",
+              description: "Your qualys_was Console port, e.g. 8080" },
+            { name: "qualys_was_user",
               type: "user",
               required: true,
               default: nil,
-              description: "Aqua Username" },
-            { name: "aqua_password",
+              description: "qualys_was Username" },
+            { name: "qualys_was_password",
               type: "password",
               required: true,
               default: nil,
-              description: "Aqua Password" },
+              description: "qualys_was Password" },
             { name: "container_data",
               type: "boolean",
-              required: true,
+              required: false,
               default: "false",
               description: "Optional filter to limit vulnerabilities using a comma separated list of severities (e.g. CRITICAL,HIGH)" },
             { name: "kenna_api_key",
@@ -57,7 +57,7 @@ module Kenna
             { name: "output_directory",
               type: "filename",
               required: false,
-              default: "output/aqua",
+              default: "output/qualys_was",
               description: "If set, will write a file upon completion. Path is relative to #{$basedir}" }
           ]
         }
@@ -66,14 +66,14 @@ module Kenna
       def run(opts)
         super # opts -> @options
 
-        username = @options[:aqua_user]
-        password = @options[:aqua_password]
-        aqua_port = @options[:aqua_console_port]
-        aqua_console = @options[:aqua_console]
-        aqua_url = if aqua_port
-                     "#{aqua_console}:#{aqua_port}"
+        username = @options[:qualys_was_user]
+        password = @options[:qualys_was_password]
+        qualys_was_port = @options[:qualys_was_console_port]
+        qualys_was_console = @options[:qualys_was_console]
+        qualys_was_url = if qualys_was_port
+                     "#{qualys_was_console}:#{qualys_was_port}"
                    else
-                     aqua_console
+                     qualys_was_console
                    end
         container_data = @options[:container_data]
 
@@ -84,168 +84,183 @@ module Kenna
         @kenna_api_key = @options[:kenna_api_key]
         @kenna_connector_id = @options[:kenna_connector_id]
 
-        token = qualys_was_get_token(aqua_url, username, password)
-
-        if container_data
-          print_debug "Container_data flag set to true"
-          containers = {}
-          contpages = true
-          while contpages
-
-            cont_pagenum += 1
-            cont_json = aqua_get_containers(aqua_url, token, 500, cont_pagenum)
-
-            if cont_json.nil? || cont_json.empty? || cont_json.length.zero?
-              contpages = false
-              break
-            end
-
-            cont_json.each do |cont_obj|
-              cont_id = cont_obj["id"]
-              cont_name = cont_obj["name"]
-              # host_name = cont_obj["host_name"]
-              img_id = cont_obj.fetch("image_id")
-              # img_name = cont_obj["image_name"]
-              cont_status = cont_obj["status"]
-              sys_cont = cont_obj["system_container"]
-              cont_type = cont_obj["container_type"]
-              enforcer_group = cont_obj["host_enforcer_group"]
-              compliant = cont_obj["compliant"]
-              img_compliant = cont_obj["image_assurance_compliant"]
-
-              # cont_locator = {
-              #   "container_id" => cont_id
-              # }
-
-              cont_asset = {
-                "container_id" => cont_id,
-                "asset_type" => "container",
-                "hostname" => cont_name,
-                "image_id" => img_id,
-                "tags" => ["status: #{cont_status}",
-                           "systemContainer: #{sys_cont}",
-                           "type: #{cont_type}",
-                           "enforcerGroup: #{enforcer_group}",
-                           "containerCompliance: #{compliant}",
-                           "imageCompliance: #{img_compliant}"]
-              }
-              print_debug "Creating a Container HashMap"
-              containers.store(img_id, cont_id)
-              print_debug "Creating Container asset"
-              create_kdi_asset(cont_asset)
-            end
-          end
+        token = qualys_was_get_token(username, password)
+        web_apps = qualys_was_get_webapp(token)
+        web_apps_findings = {}
+        web_apps['ServiceResponse']['data'].each do |web_app|
+          web_apps_findings[web_app['WebApp']['id']] = qualys_was_get_webapp_findings(web_app['WebApp']['id'], token)
         end
 
-        morepages = true
-        while morepages
+        web_apps_findings.each do |web_app, findings|
+          p "QID Details For WebApp ID: #{web_app}"
+          qids = findings['ServiceResponse']['data'].map{|x| x['Finding']['qid']}
 
-          pagenum += 1
-          vuln_json = aqua_get_vuln(aqua_url, token, 500, pagenum)
+          qualys_was_get_vuln(qids, token)
+        end
 
-          # print_debug "vuln json = #{vuln_json}"
-          print_debug "Page: #{pagenum}"
-          vuln_json.to_json
-          # print_debug "vuln result json: #{vuln_result_json}"
+        #if container_data
+        #  print_debug "Container_data flag set to true"
+        #  containers = {}
+        #  contpages = true
+        #  while contpages
+#
+#            cont_pagenum += 1
+#            cont_json = qualys_was_get_containers(qualys_was_url, token, 500, cont_pagenum)
 
-          if vuln_json.nil? || vuln_json.empty? || vuln_json.length.zero?
-            morepages = false
-            break
-          end
+#            if cont_json.nil? || cont_json.empty? || cont_json.length.zero?
+#              contpages = false
+#              break
+#            end
+#
+#            cont_json.each do |cont_obj|
+#              cont_id = cont_obj["id"]
+#              cont_name = cont_obj["name"]
+#              # host_name = cont_obj["host_name"]
+#              img_id = cont_obj.fetch("image_id")
+              # img_name = cont_obj["image_name"]
+#              cont_status = cont_obj["status"]
+#              sys_cont = cont_obj["system_container"]
+#              cont_type = cont_obj["container_type"]
+#              enforcer_group = cont_obj["host_enforcer_group"]
+#              compliant = cont_obj["compliant"]
+#              img_compliant = cont_obj["image_assurance_compliant"]
+#
+#              cont_locator = {
+#                "container_id" => cont_id
+#              }
+#
+#              cont_asset = {
+#                "container_id" => cont_id,
+#                "asset_type" => "container",
+#                "hostname" => cont_name,
+#                "locator_fields" => cont_locator,
+#                "tags" => ["status: #{cont_status}",
+#                           "systemContainer: #{sys_cont}",
+#                           "type: #{cont_type}",
+#                           "enforcerGroup: #{enforcer_group}",
+#                           "containerCompliance: #{compliant}",
+#                           "imageCompliance: #{img_compliant}",
+#                           "imageID: #{img_id}"]
+#              }
+#              print_debug "Creating a Container HashMap"
+#              containers.store(img_id, cont_id)
+#              print_debug "Creating Container asset"
+#              create_kdi_asset(cont_asset)
+#            end
+#          end
+#        end
+
+#        morepages = true
+#        while morepages
+#
+#          pagenum += 1
+#          vuln_json = qualys_was_get_vuln(qualys_was_url, token, 500, pagenum)
+#
+#          # print_debug "vuln json = #{vuln_json}"
+#          print_debug "Page: #{pagenum}"
+#          vuln_json.to_json
+#          # print_debug "vuln result json: #{vuln_result_json}"
+#
+#          if vuln_json.nil? || vuln_json.empty? || vuln_json.length.zero?
+#            morepages = false
+#            break
+#          end
 
           # Not sure if needed
           # finding_severity = { "high" => 6, "medium" => 4, "low" => 1 }
-          vuln_json.each do |vuln_obj|
-            vuln_name = vuln_obj["name"]
-            identifiers = vuln_obj["name"]
-            resource_obj = vuln_obj["resource"]
-            package_manager = resource_obj.fetch("format") if resource_obj.key?("format")
-            package = resource_obj.fetch("name") if resource_obj.key?("name")
-            # version =  resource_obj.fetch("version") if resource_obj.key?("version")
-            image_name = vuln_obj["image_name"]
-            image_id = vuln_obj["image_digest"]
-            image_registry = vuln_obj["registry"]
-            image_repo = vuln_obj["image_repository_name"]
-            os = "#{vuln_obj['os']}-#{vuln_obj['os_version']}" if vuln_obj.key?("os_version")
-            arch = resource_obj.fetch("arch") if resource_obj.key?("arch")
-            ack_date = vuln_obj["acknowledged_date"]
-            aqua_score = (vuln_obj["aqua_score"]).ceil
-            print_debug "Vuln name: #{vuln_name}"
+#          vuln_json.each do |vuln_obj|
+#            vuln_name = vuln_obj["name"]
+#            identifiers = vuln_obj["name"]
+#            resource_obj = vuln_obj["resource"]
+#            package_manager = resource_obj.fetch("format") if resource_obj.key?("format")
+#            package = resource_obj.fetch("name") if resource_obj.key?("name")
+#            # version =  resource_obj.fetch("version") if resource_obj.key?("version")
+#            image_name = vuln_obj["image_name"]
+#            image_id = vuln_obj["image_digest"]
+#            image_registry = vuln_obj["registry"]
+#            image_repo = vuln_obj["image_repository_name"]
+#            os = "#{vuln_obj['os']}-#{vuln_obj['os_version']}" if vuln_obj.key?("os_version")
+#            arch = resource_obj.fetch("arch") if resource_obj.key?("arch")
+#            ack_date = vuln_obj["acknowledged_date"]
+#            qualys_was_score = (vuln_obj["qualys_was_score"]).ceil
+#            print_debug "Vuln name: #{vuln_name}"
 
-            # locator = {
-            #   "image_id" => image_id
-            # }
+#            locator = {
+#              "image_id" => image_id
+#            }
 
-            img_asset = {
-
-              "image_id" => image_id,
-              "asset_type" => "image",
-              "hostname" => image_name,
-              "os" => os,
-              "tags" => ["registry: #{image_registry}",
-                         "repository: #{image_repo}",
-                         "architecture: #{arch}",
-                         "package: #{package}",
-                         "packageManager: #{package_manager}",
-                         "acknowledged_date: #{ack_date}"]
-            }
+#            img_asset = {
+#
+#              "image_id" => image_id,
+#              "asset_type" => "image",
+#              "hostname" => image_name,
+#              "locator_fields" => locator,
+#              "os" => os,
+#              "tags" => ["registry: #{image_registry}",
+#                         "repository: #{image_repo}",
+#                         "architecture: #{arch}",
+#                         "package: #{package}",
+#                         "packageManager: #{package_manager}",
+#                         "acknowledged_date: #{ack_date}"]
+#            }
             # print_debug asset
 
-            scanner_score = aqua_score
-            description = vuln_obj.fetch("description") if vuln_obj.key?("description")
-            solution = vuln_obj.fetch("solution") if vuln_obj.key?("solution")
-            cve_identifiers = identifiers if identifiers.include? "CVE"
+#            scanner_score = qualys_was_score
+#            description = vuln_obj.fetch("description") if vuln_obj.key?("description")
+#            solution = vuln_obj.fetch("solution") if vuln_obj.key?("solution")
+#            cve_identifiers = identifiers if identifiers.include? "CVE"
 
             # craft vuln def
-            vuln_def = {
-              "scanner_type" => "Aqua",
-              "name" => vuln_name,
-              "description" => description,
-              "solution" => solution,
-              "cve_identifiers" => cve_identifiers
-            }
-            vuln_def.compact!
+#            vuln_def = {
+#              "scanner_type" => "qualys_was",
+#              "name" => vuln_name,
+#              "description" => description,
+#              "solution" => solution,
+#              "cve_identifiers" => cve_identifiers,
+#              "scanner_identifier" => identifiers
+#            }
+#            vuln_def.compact!
             # print_debug vuln_def
 
             # craft the vuln hash
-            vuln = {
-              "scanner_identifier" => identifiers,
-              "scanner_type" => "Aqua",
-              "scanner_score" => scanner_score,
-              "created_at" => vuln_obj.fetch("first_found_date"),
-              "last_seen_at" => vuln_obj.fetch("last_found_date"),
-              "status" => "open",
-              "vuln_def_name" => vuln_name
-            }
+#            vuln = {
+#              "scanner_identifier" => identifiers,
+#              "scanner_type" => "qualys_was",
+#              "scanner_score" => scanner_score,
+#              "created_at" => vuln_obj.fetch("first_found_date"),
+#              "last_seen_at" => vuln_obj.fetch("last_found_date"),
+#              "status" => "open",
+#              "vuln_def_name" => vuln_name
+#            }
 
-            vuln.compact!
+#            vuln.compact!
             # print_debug vuln
 
             # Create the KDI entries
-            print_debug "Creating Image Asset-Vuln in KDI"
-            create_kdi_asset_vuln(img_asset, vuln)
-
-            if container_data && containers.key?("image_id")
-              asset_id = containers.fetch(image_id)
-              print_debug "Container asset: #{asset_id}"
-              print_debug "Creating Container Asset-Vuln in KDI"
-              create_kdi_asset_vuln({ "container_id" => asset_id }, vuln, "container_id")
-            end
-
-            print_debug "Creating Asset-Vuln in KDI"
-            create_kdi_vuln_def(vuln_def)
-          end
-        end
-
-        ### Write KDI format
-        output_dir = "#{$basedir}/#{@options[:output_directory]}"
-        filename = "aqua_kdi.json"
-        # write_file_stream(output_dir, filename, false, @assets, @vuln_defs, 1)
+#            print_debug "Creating Image Asset-Vuln in KDI"
+#            create_kdi_asset_vuln(img_asset, vuln)
+#
+#            if container_data && containers.key?("image_id")
+#              asset_id = containers.fetch(image_id)
+#              print_debug "Container asset: #{asset_id}"
+#              print_debug "Creating Container Asset-Vuln in KDI"
+#              create_kdi_asset_vuln({ "container_id" => asset_id }, vuln, "container_id")
+#            end
+#
+#            print_debug "Creating Asset-Vuln in KDI"
+#            create_kdi_vuln_def(vuln_def)
+#          end
+#        end
+#
+#        ### Write KDI format
+#        output_dir = "#{$basedir}/#{@options[:output_directory]}"
+#        filename = "qualys_was_kdi.json"
+#        # write_file_stream(output_dir, filename, false, @assets, @vuln_defs, 1)
         # print_good "Output is available at: #{output_dir}/#{filename}"
         # print_good "Attempting to upload to Kenna API"
         # upload_file_to_kenna_connector @kenna_connector_id, @kenna_api_host, @kenna_api_key, "#{output_dir}/#{filename}", true
-        kdi_upload output_dir, filename, @kenna_connector_id, @kenna_api_host, @kenna_api_key, false, 3, 1
-        kdi_connector_kickoff @kenna_connector_id, @kenna_api_host, @kenna_api_key
+#        kdi_upload output_dir, filename, @kenna_connector_id, @kenna_api_host, @kenna_api_key, false, 3, 1
+#        kdi_connector_kickoff @kenna_connector_id, @kenna_api_host, @kenna_api_key
       end
     end
   end
